61b41641d7939471ab37225fb860480c58213c21
metinsay <metinnsay@gmail.com>
2017-07-22 14:27:49 -0400

    Some minor changes

--- src/utils/constants/distance_metric.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from enum import Enum
-from numpy import *
-import numpy as np
-
-class DistanceMetric(Enum):
-    eucledian = staticmethod(lambda v1, v2 : sqrt(np.sum(square(array(v1) - array(v2)), axis=1)))
-    manhattan = staticmethod(lambda v1, v2 : sum(absolute(v1 - v2), axis=1))
-    angular = staticmethod(lambda v1, v2 : 2 * arccos(einsum('ij,ij->i', v1, v2) / (norm(p1, axis=1) * norm(p2, axis=1))) / pi)
--- src/utils/dimension_reduction/dimension_reducer.py
+++
@@ -1,8 +0,0 @@
-
-class DimensionReducer(object):
-
-    def __init__(self, n_component=2):
-        pass
-
-    def reduce(self, data):
-        pass

79e5df2e90a0021dedfb5abcd2d22879fc21b7ad
metinsay <metinnsay@gmail.com>
2017-08-02 23:17:55 -0400

    BST draft

--- docluster/main.py
+++ docluster/main.py
@@ -10,8 +10,20 @@ import nltk
 import numpy as np
 from bs4 import BeautifulSoup
 from core import Preprocessor, Word2Phrase
-from utils import (DistanceMetric, FileFetcher, FileSaver, FileType, Language,
-                   Trie, TweetFetcher, WikiFetcher)
+from utils import (BST, DistanceMetric, FileFetcher, FileSaver, FileType,
+                   Language, Trie, TweetFetcher, WikiFetcher)
+
+bst = BST()
+bst.insert(15, None)
+bst.insert(10, None)
+bst.insert(20, None)
+bst.insert(13, None)
+bst.insert(14, None)
+bst.insert(12, None)
+bst.insert(11, None)
+bst.insert(9, None)
+bst.insert(40, None)
+bst.insert(17, None)
 
 # Creating the model
 # import pickle
@@ -21,24 +33,24 @@ from utils import (DistanceMetric, FileFetcher, FileSaver, FileType, Language,
 #     print(data)
 
 #
-wiki_directories = ['/Users/metinsay/Downloads/wikiextractor-master/text/AA']
-onlyfiles = []
-for directory in wiki_directories:
-    onlyfiles.extend([join(directory, f)
-                      for f in listdir(directory) if isfile(join(directory, f))])
-
-
-documents = []
-for file_ in onlyfiles[:3]:
-    documents.append(BeautifulSoup(open(file_, 'r').read(), "lxml").get_text())
-
-
-w2p = Word2Phrase(max_phrase_len=4)
-w2p.fit(documents)
-print(w2p.phrases)
-documents = w2p.put_phrases_in_documents(documents)
-
-tokens = Preprocessor().fit(documents[0])
+# wiki_directories = ['/Users/metinsay/Downloads/wikiextractor-master/text/AA']
+# onlyfiles = []
+# for directory in wiki_directories:
+#     onlyfiles.extend([join(directory, f)
+#                       for f in listdir(directory) if isfile(join(directory, f))])
+#
+#
+# documents = []
+# for file_ in onlyfiles[:3]:
+#     documents.append(BeautifulSoup(open(file_, 'r').read(), "lxml").get_text())
+#
+#
+# w2p = Word2Phrase(max_phrase_len=4)
+# w2p.fit(documents)
+# print(w2p.phrases)
+# documents = w2p.put_phrases_in_documents(documents)
+#
+# tokens = Preprocessor().fit(documents[0])
 # #
 # # w2v = Word2Vec(n_workers=8)
 # #
@@ -62,4 +74,4 @@ tokens = Preprocessor().fit(documents[0])
 #
 # model = gensim.models.Word2Vec(clustered_data, size=300,
 #                                window=10, min_count=0, workers=8, sg=1, iter=5)
-trie = Trie(tokens)
+# trie = Trie(tokens)

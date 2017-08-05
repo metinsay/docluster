c987c011b75ef94ef26755b6e658e1b01cb8f4e0
metinsay <metinnsay@gmail.com>
2017-08-05 01:16:44 -0400

    Finished a draft of the class

--- docluster/utils/data_structures/huffman_tree.py
+++ docluster/utils/data_structures/huffman_tree.py
@@ -1,4 +1,4 @@
-from utils import Heap
+from heapq import heapify, heappop, heappush
 
 # Inspired by https://rosettacode.org/wiki/Huffman_coding#Python
 
@@ -7,23 +7,29 @@ class HuffmanTree(object):
 
     def __init__(self, frequencies):
         self.frequencies = frequencies
+        self.encodings = self._create_tree()
 
     def __init__(self, iterable):
         self.frequencies = self._calculate_frequencies(iterable)
+        self.encodings = self._create_tree()
 
     def _calculate_frequencies(self, iterable):
         freqs = {}
-        for element in iterable:
-            freqs[element] = freqs.get(element, 0) + 1
+        for item in iterable:
+            freqs[item] = freqs.get(item, 0) + 1
         return freqs
 
     def _create_tree(self):
-        items = [(freq, (item, '')) for item, freq in self.frequencies.items()]
-        heap = Heap(items)
+        heap = [[freq, [item, '']] for item, freq in list(self.frequencies.items())]
+        heapify(heap)
         while len(heap) > 1:
-            min1, min2 = heap.pop(), heap.pop()
+            min1, min2 = heappop(heap), heappop(heap)
             min1[1:] = [[item[0], item[1] + '0'] for item in min1[1:]]
             min2[1:] = [[item[0], item[1] + '1'] for item in min2[1:]]
             merged_item = [min1[0] + min2[0]] + min1[1:] + min2[1:]
-            heap.insert(merged_item)
-        return heap
+            heappush(heap, merged_item)
+        encodings_list = heap[0][1:]
+        return {pair[0]: pair[1] for pair in encodings_list}
+
+    def encode(self, iterable):
+        return ''.join(map(lambda item: self.encodings[item], iterable))

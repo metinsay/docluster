48c472b00ff8ed520b2a8dfef5d0b7ea8d2987e4
metinsay <metinnsay@gmail.com>
2017-08-05 01:34:31 -0400

    Decoding a Huffman encoding

--- docluster/utils/data_structures/huffman_tree.py
+++ docluster/utils/data_structures/huffman_tree.py
@@ -24,8 +24,8 @@ class HuffmanTree(object):
         heapify(heap)
         while len(heap) > 1:
             min1, min2 = heappop(heap), heappop(heap)
-            min1[1:] = [[item[0], item[1] + '0'] for item in min1[1:]]
-            min2[1:] = [[item[0], item[1] + '1'] for item in min2[1:]]
+            min1[1:] = [[item[0], '0' + item[1]] for item in min1[1:]]
+            min2[1:] = [[item[0], '1' + item[1]] for item in min2[1:]]
             merged_item = [min1[0] + min2[0]] + min1[1:] + min2[1:]
             heappush(heap, merged_item)
         encodings_list = heap[0][1:]
@@ -33,3 +33,14 @@ class HuffmanTree(object):
 
     def encode(self, iterable):
         return ''.join(map(lambda item: self.encodings[item], iterable))
+
+    def decode(self, code):
+        reverse_encodings = dict(zip(self.encodings.values(), self.encodings.keys()))
+        cur = ''
+        decoded_items = []
+        for binary in code:
+            cur += binary
+            if cur in reverse_encodings:
+                decoded_items.append(reverse_encodings[cur])
+                cur = ''
+        return decoded_items

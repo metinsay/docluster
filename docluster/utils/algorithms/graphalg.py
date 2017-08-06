962a135ac70b2ff20448d487774f3eed310f7f7d
metinsay <metinnsay@gmail.com>
2017-08-06 15:57:21 -0400

    Outline for graph algorithms

---
+++ docluster/utils/algorithms/graphalg.py
@@ -0,0 +1,52 @@
+""" CHECKS """
+
+
+def check_cycles(graph):
+    return False
+
+
+""" SEARCH """
+
+
+def dfs(graph, search):
+    pass
+
+
+def bfs(graph, search):
+    pass
+
+
+""" TRAVERSAL """
+
+
+def dfs_traverse(graph):
+    pass
+
+
+def bfs_traverse(graph):
+    pass
+
+
+""" SORT """
+
+
+""" SHORTEST PATH """
+
+
+def dijstra(graph, source):
+    pass
+
+
+def bellman_ford(graph, source):
+    pass
+
+
+def floyd_warshall(graph):
+    pass
+
+
+def johnson(graph):
+    pass
+
+
+""" CONNECTIVITY """
